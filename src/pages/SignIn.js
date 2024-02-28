09f961b32a073e11cf2d7437239edd5531d72ac1
heeyeon <dlgmldus3289@naver.com>
2024-02-28 17:15:56 +0900

    [refactor]Input 수정, tailwindcss 그룹화

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -6,41 +6,26 @@ const SignIn = () => {
     navigate("/signup");
   };
   return (
-    <div className="w-[900px] h-[700px] flex flex-row my-auto">
-      {/* <form className="container sign-in"> */}
-      <form className="flex flex-col w-2/4 h-full justify-center items-center border border-solid border-[#976ec2] gap-16">
+    <div className="sign-container">
+      <form className="sign-screen gap-16">
         <div className="text-5xl font-extrabold text-[#976ec2] mb-10">
           Sign in
         </div>
-        <input
-          placeholder="email"
-          type="email"
-          className="w-72 h-12 border border-1 border-solid border-[#976ec2] rounded-xl text-sm focus:outline-none"
-        />
-        <input
-          placeholder="password"
-          type="password"
-          className="w-72 h-12 border border-1 border-solid border-purple-600 rounded-xl text-sm focus:outline-none"
-        />
-        <button
-          className="w-72 h-12 rounded-xl bg-[#976ec2] text-white text-lg font-semibold"
-          type="submit"
-        >
+        <input placeholder="email" type="email" className="sign-input" />
+        <input placeholder="password" type="password" className="sign-input" />
+        <button className="sign-btn" type="submit">
           Sign in
         </button>
       </form>
 
-      <div className="flex flex-col w-2/4 h-full justify-center items-center border border-solid border-[#976ec2] bg-purple bg-[#e5d1fa] gap-14">
+      <div className="sign-screen bg-purple bg-[#e5d1fa] gap-14">
         <div
           className="text-7xl font-extrabold italic text-[#976ec2]"
           style={{ WebkitTextStroke: "2px #FFF" }}
         >
           pl&o
         </div>
-        <button
-          className="w-72 h-12 rounded-xl bg-[#976ec2] text-white text-lg font-semibold"
-          onClick={goSignUp}
-        >
+        <button className="sign-btn" onClick={goSignUp}>
           sign up
         </button>
       </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,40 +1,24 @@
 const SignUp = () => {
   return (
-    <div className="w-[900px] h-[700px] flex flex-row my-auto">
-      <form className="flex flex-col w-2/4 h-full justify-center items-center border border-solid border-[#976ec2] gap-10">
+    <div className="sign-container">
+      <form className="sign-screen gap-10">
         <div className="text-5xl font-extrabold text-[#976ec2] mb-10">
           Sign up
         </div>
-        <input
-          placeholder="nickname"
-          type="name"
-          className="w-72 h-12 border border-1 border-solid border-[#976ec2] rounded-xl text-sm"
-        />
-        <input
-          placeholder="email"
-          type="email"
-          className="w-72 h-12 border border-1 border-solid border-[#976ec2] rounded-xl text-sm"
-        />
+        <input placeholder="nickname" type="name" className="sign-input" />
+        <input placeholder="email" type="email" className="sign-input" />
         <input
           placeholder="password"
           type="password"
-          className="w-72 h-12 border border-1 border-solid border-[#976ec2] rounded-xl text-sm
+          className="sign-input
         "
         />
-        <input
-          placeholder="password"
-          type="password"
-          className="w-72 h-12 border border-1 border-solid border-[#976ec2] rounded-xl text-sm"
-        />
-        <button
-          className="w-72 h-12 rounded-xl bg-[#976ec2] text-white text-lg font-semibold"
-          c
-          type="submit"
-        >
+        <input placeholder="password" type="password" className="sign-input" />
+        <button className="sign-btn" type="submit">
           Sign up
         </button>
       </form>
-      <div className="flex flex-col w-2/4 h-full justify-center items-center border border-solid border-[#976ec2] bg-[#e5d1fa] gap-14">
+      <div className="sign-screen bg-[#e5d1fa] gap-14">
         <div
           className="text-7xl font-extrabold italic text-[#976ec2]"
           style={{ WebkitTextStroke: "2px #FFF" }}
