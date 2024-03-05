9c65080885483e6a9174e9c512709b4d66e8bca4
heeyeon <dlgmldus3289@naver.com>
2024-03-05 22:09:50 +0900

    [feat]로그인 유효성 검사

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -9,17 +9,47 @@ const SignIn = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="sign-container">
-      <form className="sign-screen gap-16">
+      <form className="sign-screen" onSubmit={handleSubmit(onSubmit)}>
         <div className="text-5xl font-extrabold text-[#976ec2] mb-10">
           Sign in
         </div>
-        <input placeholder="email" type="email" className="sign-input" />
-        <input placeholder="password" type="password" className="sign-input" />
+        <input
+          placeholder="email"
+          type="email"
+          className="sign-input"
+          {...register("email", {
+            required: "필수 입력 항목입니다.",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "올바른 이메일 형식이 아닙니다.",
+            },
+          })}
+        />
+        <div className="sign-error-message">
+          {errors.email && errors.email.message}
+        </div>
+        <input
+          placeholder="password"
+          type="password"
+          className="sign-input"
+          {...register("password", {
+            required: "필수 입력 항목입니다.",
+            minLength: {
+              value: 6,
+              message: "비밀번호는 최소 6자 이상이어야 합니다.",
+            },
+          })}
+        />
+        <div className="sign-error-message">
+          {errors.password && errors.password.message}
+        </div>
         <button className="sign-btn" type="submit">
           Sign in
         </button>
