9f661200acbf70678a6f47af214eb001f9a2bcef
Hj1218 <wjdguswl2341@naver.com>
2024-03-19 22:44:39 +0900

    [design] 일기 없는 경우 일기작성 버튼 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Calendar from "../component/Main/Calendar";
 import Todo from "../component/Main/Todo/Todo";
+import NoDiary from "../component/Main/Diary/NoDiary";
 const Main = () => {
   return (
     <div className={"w-full"}>
       <Calendar />
       <Todo />
+      <NoDiary />
     </div>
   );
 };
