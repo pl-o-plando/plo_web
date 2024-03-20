f8ec0574f8fa98320f794aaab2126430c976025a
Hj1218 <wjdguswl2341@naver.com>
2024-03-20 18:50:07 +0900

    [design] 디자인 수정, 일기작성 버튼 클릭 시 diaryshow 컴포넌트로 변경

--- src/component/Main/Diary/DiaryShow.js
+++ src/component/Main/Diary/DiaryShow.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCheck } from "react-icons/fa";
 
 const DiaryShow = () => {
-  return(
+  const maxLines = 19;
+  const maxCharacters = 912;
+
+  const [mood, setMood] = useState('');
+  const [diaryContent, setDiaryContent] = useState('일기를 작성해주세요.');
+  const [newDiary, setNewDiary] = useState('');
+  const [isDiaryChanged, setIsDiaryChanged] = useState(false);
+  const [inputContent, setInputContent] = useState('');
+
+  const handleSaveDiary = () => {
+    setDiaryContent(newDiary);
+    setIsDiaryChanged(true);
+    setInputContent(mood);
+  };
+
+  const handleDiaryChange = (event) => {
+    const text = event.target.value;
+    const lines = text.split('\n').length;
+    if (lines <= maxLines && text.length <= maxCharacters) {
+      setNewDiary(text);
+    }
+  };
+
+  return (
     <div className="flex flex-col mx-5">
       <div className="text-[#976EC2] text-5xl font-bold justify-start mb-14">Diary</div>
       <div className="border-[#C78ED6] border-2 rounded-xl h-[4rem] flex mb-10">
-        <div className="bg-[#E5D1FA] text-[#976EC2] w-[12%] flex justify-center items-center text-3xl font-bold">기분</div>
-        <input className="flex-1"></input>
+        <div className="bg-[#E5D1FA] text-[#976EC2] w-[12%] flex justify-center items-center text-3xl font-bold rounded-l-xl border-r-2 border-[#C78ED6]">기분</div>
+        <input 
+          maxLength="45" 
+          value={mood}
+          onChange={(event) => setMood(event.target.value)}
+          placeholder={isDiaryChanged ? inputContent : "기분을 입력해주세요."}
+          className="flex-1 rounded-r-xl text-xl p-3 outline-[#8148bd]"
+        />
       </div>
-      <input placeholder="일기를 작성해주세요." className="border-[#C78ED6] border-2 h-[40rem] rounded-lg mb-10"></input>
+      <textarea
+        value={newDiary}
+        onChange={handleDiaryChange}
+        placeholder={isDiaryChanged ? diaryContent : "일기를 작성해주세요."}
+        rows={maxLines}
+        maxLength={maxCharacters}
+        className="border-[#C78ED6] border-2 h-[40rem] rounded-lg mb-10 p-5 text-2xl resize-none outline-[#8148bd] overflow-hidden"
+      ></textarea>
       <div className="flex justify-end">
-        <div className="bg-[#976EC2] w-[8rem] h-[4rem] rounded-xl text-white font-bold text-2xl flex justify-center items-center">저장</div>
+        <div 
+          className="bg-[#976EC2] w-[8rem] h-[4rem] rounded-xl text-white font-bold text-2xl flex justify-center items-center"
+          onClick={handleSaveDiary}
+        >
+          <FaCheck color="white" size={35} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DiaryShow;
--- src/component/Main/Diary/NoDiary.js
+++ src/component/Main/Diary/NoDiary.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-const NoDiary = () => {
-  const navigate = useNavigate();
-
-  const handleDiaryClick = () => {
-    navigate("/diary");
-  };
-
+const NoDiary = ({ onDiaryButtonClick }) => {
   return (
     <div className="flex items-center justify-between px-8">
       <div className="text-xl text-[#888888] mb-10">작성된 일기가 없습니다.</div>
-      <div className="mb-10 bg-[#976EC2] text-xl text-white w-[10rem] h-[3rem] justify-center flex items-center rounded font-bold" onClick={handleDiaryClick}>일기작성</div>
+      <div className="mb-10 bg-[#976EC2] text-xl text-white w-[10rem] h-[3rem] justify-center flex items-center rounded font-bold" onClick={onDiaryButtonClick}>일기작성</div>
     </div>
   );
 };
