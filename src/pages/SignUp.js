a989004f9069599c156163273b55d53fd6948d17
heedong12 <dlgmldus3289@naver.com>
2024-03-23 20:37:36 +0900

    [refactor] 이메일 input type 수정

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -33,7 +33,7 @@ const SignUp = () => {
         <div>
           <input
             placeholder="email"
-            type="email"
+            type="text"
             className="sign-input ml-12 mr-4"
             {...register("email", {
               required: "필수 입력 항목입니다.",
@@ -43,7 +43,10 @@ const SignUp = () => {
               },
             })}
           />
-          <button type="button" className={"text-[#976EC2] font-semibold h-12"}>
+          <button
+            type="button"
+            className={"text-[#976EC2] font-extrabold h-12"}
+          >
             확인
           </button>
         </div>
