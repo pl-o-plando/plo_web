c9c7fb13c77b6d9444032feb9bc068dd0b749321
Hj1218 <wjdguswl2341@naver.com>
2024-03-26 20:51:14 +0900

    [design] pie chart 구현

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Graph from "../component/Graph";
 
-const Statistics = () =>{
-  return(
-    <div><Graph/></div>
-  )
-}
+const Statistics = () => {
+  const graphData = [12, 5, 3];
+
+  return (
+    <div className="w-full h-[28rem] flex justify-center items-end">
+      <Graph data={graphData}/>
+    </div>
+  );
+};
 
 export default Statistics;
